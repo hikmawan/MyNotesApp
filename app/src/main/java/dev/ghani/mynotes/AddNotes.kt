dd64d6a66ab8dc775b63fba9be2e435664961d22
Ghani Hikmawan <ghani@paxel.co>
2020-05-04 16:07:51 +0700

    use menu bar and intent

--- app/src/main/java/dev/ghani/mynotes/AddNotes.kt
+++ app/src/main/java/dev/ghani/mynotes/AddNotes.kt
@@ -2,11 +2,16 @@ package dev.ghani.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AddNotes : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+    }
+
+    fun buAdd(view: View){
+        finish()
     }
 }
